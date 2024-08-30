a0ea62d7bc2754ea6da94e756005ca6de960d882
Adam Howard <adam.thomas.howard@gmail.com>
2024-08-30 21:44:22 +0200

    Test cache

--- server/api/page/info.post.ts
+++ server/api/page/info.post.ts
@@ -1,7 +1,9 @@
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
     const {siteId, path} = await readBody(event)
 
     const page = await getPageInfo(siteId, path)
+
+    console.log(`Loaded page at ${path}`)
 
     if (!page) throw createError({
         statusCode: 404,
@@ -9,4 +11,7 @@ export default defineEventHandler(async (event) => {
     })
 
     return page;
+}, {
+    maxAge: 60,
+    name: 'pageInfo'
 })
