cba8592a4899ed1a0b71310d1cda4f7ccbd1281d
Adam Howard <adam.thomas.howard@gmail.com>
2024-09-04 20:32:48 +0200

    Debug

--- composables/subdomain.ts
+++ composables/subdomain.ts
@@ -1,10 +1,12 @@
-import type { Page } from '@canopie-club/prisma-client'
-import type { PageRoute, SiteWithPagePaths } from "~~/utils/site"
+import type { Page } from '~/assets/types/db'
 import type { Profiles } from "~~/utils/types"
+// import type { PageRoute, SiteWithPagePaths } from "~~/utils/site"
 
 export const useSubdomain = () => useState<string | null>("subdomain", () => null)
 export const useSubdomainProfile = () => useState<Profiles | null>("subdomain-profile", () => null)
-export const useSiteInfo = () => useState<SiteWithPagePaths | null>("site-info", () => null)
+// TODO: Fix any
+export const useSiteInfo = () => useState<any | null>("site-info", () => null)
 export const usePageInfo = () => useState<Page | null>("page-info", () => null)
-export const useMenuRoutes = () => useState<PageRoute[]>("menu-routes", () => [])
+// TODO: Fix any
+export const useMenuRoutes = () => useState<any[]>("menu-routes", () => [])
 export const useSpaContent = () => useState<string | null>("spa-content", () => null)
