481d1a28507a7f05f2e4e83c89b0748c7f8c8966
Adam Howard <adam.thomas.howard@gmail.com>
2024-09-05 07:41:55 +0200

    Test without rerouting

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,67 +2,63 @@ import type { RouterConfig } from '@nuxt/schema'
 
 export default {
     // https://router.vuejs.org/api/interfaces/routeroptions.html#routes
-    // routes: (_routes) => {
+    routes: (_routes) => {
+    // routes: (routes) => {
+    //     console.log(routes)
 
     //     const { ssrContext } = useNuxtApp()
     //     const subdomain = useSubdomain()
+    //     const siteInfo = useSiteInfo()
+    //     const pageInfo = usePageInfo()
+    //     const menuRoutes = useMenuRoutes()
+    //     const spaContent = useSpaContent()
+
+    //     console.log("setting context")
+
     //     if (ssrContext?.event.context.subdomain) subdomain.value = ssrContext?.event.context.subdomain
-    // },
-    routes: (routes) => {
-        console.log(routes)
+    //     if (ssrContext?.event.context.siteInfo) siteInfo.value = ssrContext?.event.context.siteInfo
+    //     if (ssrContext?.event.context.pageInfo) pageInfo.value = ssrContext?.event.context.pageInfo
+    //     if (ssrContext?.event.context.menuRoutes) menuRoutes.value = ssrContext?.event.context.menuRoutes
+    //     if (ssrContext?.event.context.spaContent) spaContent.value = ssrContext?.event.context.spaContent
 
-        const { ssrContext } = useNuxtApp()
-        const subdomain = useSubdomain()
-        const siteInfo = useSiteInfo()
-        const pageInfo = usePageInfo()
-        const menuRoutes = useMenuRoutes()
-        const spaContent = useSpaContent()
+    //     console.log("context set")
 
-        console.log("setting context")
+    //     const template = siteInfo.value?.template || 'default'
 
-        if (ssrContext?.event.context.subdomain) subdomain.value = ssrContext?.event.context.subdomain
-        if (ssrContext?.event.context.siteInfo) siteInfo.value = ssrContext?.event.context.siteInfo
-        if (ssrContext?.event.context.pageInfo) pageInfo.value = ssrContext?.event.context.pageInfo
-        if (ssrContext?.event.context.menuRoutes) menuRoutes.value = ssrContext?.event.context.menuRoutes
-        if (ssrContext?.event.context.spaContent) spaContent.value = ssrContext?.event.context.spaContent
+    //     if (subdomain.value) {
+    //         console.log("filtering routes")
 
-        console.log("context set")
+    //         const userRoute = routes.filter((i) => i.path.includes("/user/:siteId"))
 
-        const template = siteInfo.value?.template || 'default'
+    //         // if (template === 'spa') {
+    //         //     const userRouteMapped = userRoute.map((i) => ({
+    //         //         ...i,
+    //         //         path: i.path === `/user/:siteId()/${template}` ? i.path.replace(`/user/:siteId()/${template}`, "/") : i.path.replace(`/user/:siteId()/${template}/`, "/#/"),
+    //         //     }))
 
-        if (subdomain.value) {
-            console.log("filtering routes")
+    //         //     console.log(userRouteMapped)
 
-            const userRoute = routes.filter((i) => i.path.includes("/user/:siteId"))
-
-            // if (template === 'spa') {
-            //     const userRouteMapped = userRoute.map((i) => ({
-            //         ...i,
-            //         path: i.path === `/user/:siteId()/${template}` ? i.path.replace(`/user/:siteId()/${template}`, "/") : i.path.replace(`/user/:siteId()/${template}/`, "/#/"),
-            //     }))
-
-            //     console.log(userRouteMapped)
-
-            //     return userRouteMapped
-            // }
+    //         //     return userRouteMapped
+    //         // }
             
-            const userRouteMapped = userRoute.map((i) => {
-                // replace /user/:siteId()/${template} OR /user/:siteId()/${template}/ with /
-                const newPath = i.path.replace(new RegExp(`\/user\/\:siteId\\(\\)\/${template}\/?`), "/")
-                if (template === 'spa') {
-                    console.log(newPath)
-                }
-                return {
-                ...i,
-                path: newPath,
-            }})
+    //         const userRouteMapped = userRoute.map((i) => {
+    //             // replace /user/:siteId()/${template} OR /user/:siteId()/${template}/ with /
+    //             const newPath = i.path.replace(new RegExp(`\/user\/\:siteId\\(\\)\/${template}\/?`), "/")
+    //             if (template === 'spa') {
+    //                 console.log(newPath)
+    //             }
+    //             return {
+    //             ...i,
+    //             path: newPath,
+    //         }})
 
-            console.log("routes mapped")
+    //         console.log("routes mapped")
 
-            // console.log(userRouteMapped)
+    //         // console.log(userRouteMapped)
 
-            return userRouteMapped
-        }
-        return routes
+    //         return userRouteMapped
+    //     }
+    //      return routes
+        return _routes
     },
 } satisfies RouterConfig
